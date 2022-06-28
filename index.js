f53881725ab70087bd2952d959aba33b3ebba741
loclink <loclink@163.com>
2022-06-29 00:22:04 +0800

    ✨ feat: 新增创建用户和打印用户列表

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 #!/usr/bin/env node
 
-const { langConfig, gitconfigData, gtrcData } = require('./lib/config');
+const config= require('./lib/config');
 const { program } = require('commander');
-const { printTable } = require('console-table-printer');
 const { registerCommands } = require('./lib/core/command');
 
 // 查看版本号
-program.version(require('./package.json').version, '-v, --version', langConfig.helpDescription.version);
+program.version(require('./package.json').version, '-v, --version',  config.languageConfig.helpDescription.version);
 
 // 自定义帮助信息
-program.helpOption('-h, --help', langConfig.helpDescription.help);
+program.helpOption('-h, --help', config.languageConfig.helpDescription.help);
 
 // 自定义子命令的帮助信息描述
-program.addHelpCommand('help [command]', langConfig.helpDescription.helpForCmd);
+program.addHelpCommand('help [command]',  config.languageConfig.helpDescription.helpForCmd);
 
 // 注册指令
 registerCommands();
 
-console.log(gtrcData);
-console.log(gitconfigData);
 
-const t = [
-  { index: 1, text: 'abcdefg' },
-  { index: 3, text: 'abcdefg' },
-  { index: 2, text: 'abcdefg' }
-];
+
 
 program.parse(process.argv);
