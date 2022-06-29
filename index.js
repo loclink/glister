472cc6242d675e3e2152de73425827060d983339
loclink <loclink@163.com>
2022-06-29 23:51:22 +0800

    ✨ feat: 新增版本校验，git校验，新增备份指令，重构部分代码

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 #!/usr/bin/env node
 
-const config= require('./lib/config');
+const config = require('./lib/config');
 const { program } = require('commander');
 const { registerCommands } = require('./lib/core/command');
+const { verifyProgramInstruction, verifyVersion } = require('./lib/utils/verify');
 
-// 查看版本号
-program.version(require('./package.json').version, '-v, --version',  config.languageConfig.helpDescription.version);
+const language = config.languageConfig;
 
-// 自定义帮助信息
-program.helpOption('-h, --help', config.languageConfig.helpDescription.help);
+// 校验git
+verifyProgramInstruction('git', language.notGit);
 
-// 自定义子命令的帮助信息描述
-program.addHelpCommand('help [command]',  config.languageConfig.helpDescription.helpForCmd);
+// 检测版本
+verifyVersion().then(res => {
+  // 查看版本号
+  program.version(require('./package.json').version, '-v, --version', language.helpDescription.version);
 
-// 注册指令
-registerCommands();
+  // 自定义帮助信息
+  program.helpOption('-h, --help', language.helpDescription.help);
 
+  // 自定义子命令的帮助信息描述
+  program.addHelpCommand('help [command]', language.helpDescription.helpForCmd);
 
+  // 注册指令
+  registerCommands();
 
-
-program.parse(process.argv);
+  program.parse(process.argv);
+});
