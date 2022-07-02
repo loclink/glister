2d0c44eeaa0722c340e6607112e29e90f36d585f
loclink <loclink@163.com>
2022-07-03 02:22:19 +0800

    ✨ feat: 重构配置加载逻辑，新增边界判断，新增表格字段，新增忽略模板，新增global和local用户

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 #!/usr/bin/env node
 
-const config = require('./lib/config');
+const reloadConfig = require('./lib/config');
 const { program } = require('commander');
 const { registerCommands } = require('./lib/core/command');
 const { helpOptions } = require('./lib/core/help');
-const { verifyProgramInstruction, verifyVersion, verifyBackup } = require('./lib/utils/verify');
+const { verifyProgramInstruction, verifyVersion, verifyIsExistGlobalUser } = require('./lib/utils/verify');
 
-const language = config.languageConfig;
+// 入口
+const glisterStart = async () => {
+  // 加载配置
+  await reloadConfig();
 
-// 校验git
-verifyProgramInstruction('git', language.notGit);
+  // 校验git
+  verifyProgramInstruction('git', program.messageInfo.notGit);
 
-// 检测版本
-verifyVersion().then(async res => {
-  // 校验当前用户是否备份
-  await verifyBackup(language.notBackup);
+  await verifyVersion();
+
+  await verifyIsExistGlobalUser();
+
+  //  注册指令
+  registerCommands();
 
   // 查看版本号
-  program.version(require('./package.json').version, '-v, --version', language.helpDescription.version);
+  program.version(require('./package.json').version, '-v, --version', program.messageInfo.helpDescription.version);
 
   // 自定义帮助信息
-  program.helpOption('-h, --help', language.helpDescription.help);
+  program.helpOption('-h, --help', program.messageInfo.helpDescription.help);
 
   // 自定义子命令的帮助信息描述
-  program.addHelpCommand('help [command]', language.helpDescription.helpForCmd);
+  program.addHelpCommand('help [command]', program.messageInfo.helpDescription.helpForCmd);
 
   // 加载额外的帮助选项
   helpOptions();
 
-  // 注册指令
-  registerCommands();
-
   program.parse(process.argv);
-});
+};
+
+glisterStart();
+// const language = config.languageConfig;
+
+// // 检测版本
+// verifyVersion().then(async res => {
+//   // 校验当前用户是否备份
+//   await verifyBackup(language.notBackup);
+
+//   );
+
+//
+
+// });
